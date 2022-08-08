c1e60a377228453831be8d0ce69dbb683a536808
rodrigoleonello <rodrigosleonello@gmail.com>
2022-08-08 15:30:45 -0300

    feat: rock-create-orogen

---
+++ deep_trekkerTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef deep_trekker_TYPES_HPP
+#define deep_trekker_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace deep_trekker {
+}
+
+#endif
+
